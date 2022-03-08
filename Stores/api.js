7260d88e943e9d1c68e09a7c8e27dca6dcb7d704
snn93 <snn93@icloud.com>
2022-03-08 11:37:48 +0300

    done

--- Stores/api.js
+++ Stores/api.js
@@ -1,11 +1,11 @@
 import axios from "axios"; //#localhost
 
 // Sara's ip's
-//export const baseURL = "http://192.168.100.151:8001"; //workplace//
+export const baseURL = "http://192.168.100.151"; //workplace//
 //export const baseURL = "http://localhost:8001";
 
 //fahad's ip's
-export const baseURL = "http://192.168.150.70:8001"; //workplace wifi
+//export const baseURL = "http://192.168.150.70:8001"; //workplace wifi
 //export const baseURL = "http://localhost:8001"; //#localhost
 //export const baseURL = "http://192.168.8.180:8001"; //myStc wifi
 
