bb8c49c8e1f7e1c0765490de9ff9fcd8ccb8f7b5
feheed <fmjuwaihel@gmail.com>
2022-03-09 09:03:55 +0300

    done msign in

--- Stores/api.js
+++ Stores/api.js
@@ -1,13 +1,13 @@
 import axios from "axios"; //#localhost
 
 // Sara's ip's
-export const baseURL = "http://192.168.8.188"; //workplace//
+//export const baseURL = "http://192.168.8.188"; //workplace//
 //export const baseURL = "http://localhost:8001";
 
 //fahad's ip's
 //export const baseURL = "http://192.168.150.70:8001"; //workplace wifi
 //export const baseURL = "http://localhost:8001"; //#localhost
-// export const baseURL = "http://192.168.8.180:8001"; //myStc wifi
+export const baseURL = "http://192.168.8.180:8001"; //myStc wifi
 
 //bashayer's ip's
 // export const baseURL = "http://192.168.150.73:8001"; //workplace wifi
